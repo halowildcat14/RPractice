0066b82c5d00a1f6974f9e933cec0fd538883928
halowildcat14 <halowildcat14@gmail.com>
2023-04-01 16:43:50 -0500

    More testing
    Submitting with new data sheet and more practice doe

--- TestScript.R
+++ TestScript.R
@@ -1,3 +1,11 @@
+library(readr)
+library(dplyr)
 add = function(a,b) {
   return (a + b)
 }
+marketdata <- read.csv("~/GitHub/RPractice/Market Data - Sheet1.csv")
+cheaper <- marketdata[which(marketdata$Low.Price > 1000), ]
+sortmarketdata <- marketdata %>% arrange(desc(Real.Item.Key))
+sortmarketdata
+marketdata
+cheaper
